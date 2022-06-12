09366b5f0dba3dda51ba64fa1380a8fed73b42b4
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-06-12 13:37:07 -0300

    Log in and sign up pages

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,6 +1,13 @@
 const postsController = require('../controllers/posts-controller')
+const usersController = require('../controllers/users-controller')
 
 module.exports = app => {
   app.get('/', postsController.getPosts)
   app.get('/post/:id', postsController.getPost)
+  app.get('/log-in', (req, res) => {
+    res.render('index', { role: 'log-in' })
+  })
+  app.get('/sign-up', (req, res) => {
+    res.render('index', { role: 'sign-up' })
+  })
 }
